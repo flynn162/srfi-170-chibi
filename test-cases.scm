d9c142de9cd086dad00a68d07d07f48ecd2b3f10
Flynn Liu <54922775+flynn162@users.noreply.github.com>
2021-01-10 15:00:32 -0800

    More environment variable tests

--- test-cases.scm
+++ test-cases.scm
@@ -12,5 +12,31 @@
   (delete-environment-variable! key)
   )
 
+(for-each
+ (lambda (kv)
+   (test-error (set-environment-variable! (car kv) (cdr kv)))
+   )
+ '(;; NUL character test
+   ("_TEST_\x00;VARIABLE" . "value")
+   ("\x00;_TEST_VARIABLE" . "value")
+   ("_TEST_VARIABLE\x00;" "value")
+   ("_TEST_VARIABLE" "val\x00;ue")
+   ("_TEST_VARIABLE" "\x00;value")
+   ("_TEST_VARIABLE" "value\x00;")
+   ;; '=' in names
+   ("MY=_TEST" "value")
+   ("=MY_TEST" "value")
+   ("MY_TEST=" "value")
+   ("=" "value")
+   ;; empty variable name
+   ("" "value")
+   ))
+
+;; Must be able to clear the value of a variable
+(set-environment-variable! "_TEST" "")
+(test-equal string=? "" (get-environment-variable "_TEST"))
+(delete-environment-variable! "_TEST")
+(test-not (get-environment-variable "_TEST"))
+
 (test-end "Additional")
 (test-exit)
